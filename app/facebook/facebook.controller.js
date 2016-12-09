81807f0c44c1c69c17f1107cea34d36c8b8f5fab
Yash <yashpatel256@gmail.com>
2016-12-09 17:37:28 -0500

    all the functionalities required for handling all the dom events

--- app/facebook/facebook.controller.js
+++ app/facebook/facebook.controller.js
@@ -6,7 +6,42 @@
 
     angular
         .module('ngSocial')
-        .controller('FacebookCtrl', ['$scope', function ($scope) {
+        .controller('FacebookCtrl', ['$scope','facebookService', function ($scope, facebookService) {
+            $scope.signIn = function () {
+                facebookService.getInfo().then(function () {
+                    $scope.isLoggedIn = true;
+                    refresh();
+                });
+            };
 
+            $scope.signOut = function () {
+                facebookService.logOut().then(function () {
+                    $scope.isLoggedIn = false;
+                    refresh();
+                });
+            };
+
+            $scope.post = function () {
+                var body = this.body;
+                facebookService.postMe(body).then(function (response) {
+                   $scope.msg = "Thanks for posting.";
+                    refresh();
+                });
+            };
+
+            function refresh() {
+              facebookService.refreshMe().then(function (response) {
+                  $scope.userInfo = response;
+                  console.log($scope.userInfo);
+                  $scope.picture = response.picture.data.url;
+                  $scope.msg = "Welcome " + response.name;
+                  $scope.permissions = response.permissions.data;
+                  $scope.posts = response.posts.data;
+              }, function (err) {
+                  $scope.msg = "Please Log in";
+              });
+            }
+
+            refresh();
         }]);
 })();
