2b888f7d7193d77c65b9826a92a6d427059021ea
Yash <yashpatel256@gmail.com>
2016-12-09 17:30:59 -0500

    managed gitignore

---
+++ app/facebook/facebook.controller.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Yash on 12/9/2016.
+ */
+(function () {
+    'use strict';
+
+    angular
+        .module('ngSocial')
+        .controller('FacebookCtrl', ['$scope', function ($scope) {
+
+        }]);
+})();
